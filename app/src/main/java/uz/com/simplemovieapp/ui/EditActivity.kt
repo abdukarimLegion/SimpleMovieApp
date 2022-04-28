ee4a512695011373e5582f598185d7c45670d940
abdukarimLegion <legionuz@inbox.ru>
2022-04-28 20:45:36 +0500

    Simple-Movie-App with RecyclerView

--- app/src/main/java/uz/com/simplemovieapp/ui/EditActivity.kt
+++ app/src/main/java/uz/com/simplemovieapp/ui/EditActivity.kt
@@ -15,7 +15,7 @@ class EditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
-
+        var gson = Gson()
         MySharedPreferences.init(this)
         var list1 = ArrayList<MovieModel>()
         var position = intent.getSerializableExtra("position") as MovieModel
@@ -23,23 +23,27 @@ class EditActivity : AppCompatActivity() {
         authors1.setText(position.authors)
         movie1.setText(position.about)
         date1.setText(position.date)
-
+        var text = MySharedPreferences.text
+        if (text!!.isNotEmpty()) {
+            var type = object : TypeToken<ArrayList<MovieModel>>() {}.type
+            list1.addAll(gson.fromJson(text, type))
+        }
         edite_btn.setOnClickListener {
-            var gson = Gson()
+
             var edite_name = edite_name1.text.toString()
             var edite_authors = authors1.text.toString()
             var movie1 = movie1.text.toString()
             var date1 = date1.text.toString()
-            var text = MySharedPreferences.text
-            if (text!!.isNotEmpty()) {
-                var type = object : TypeToken<ArrayList<MovieModel>>() {}.type
-                list1.addAll(gson.fromJson(text, type))
-            }
-            if (edite_name.trim().isEmpty() && edite_authors.trim().isNotEmpty() && movie1.trim()
-                    .isNotEmpty() && date1.trim().isNotEmpty()
+
+            if (edite_name.isNullOrBlank() && edite_authors.isNullOrBlank() && movie1
+                    .isNullOrBlank() && date1.isNullOrBlank()
             ) {
                 Toast.makeText(this, "Kino nomi mavju edmas", Toast.LENGTH_SHORT).show()
-            } else if (edite_name.trim().isNotEmpty() && edite_authors.trim().isNotEmpty() && movie1.trim().isNotEmpty() && date1.trim().isNotEmpty()) {
+            } else
+                if (edite_name.trim().isNotEmpty() && edite_authors.trim()
+                    .isNotEmpty() && movie1.trim().isNotEmpty() && date1.trim().isNotEmpty()
+            )
+            {
                 list1.removeAll(listOf(position))
                 MySharedPreferences.clear()
                 if (MySharedPreferences.text!!.isEmpty()) {
@@ -48,8 +52,10 @@ class EditActivity : AppCompatActivity() {
                 }
                 Toast.makeText(this, "Save", Toast.LENGTH_SHORT).show()
                 finish()
-            }
-            //The data is incomplete
+            }else {
+
+            Toast.makeText(this, "Iltimos bo'sh ma'lumot kiritmang", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 }
--- app/src/main/java/uz/com/simplemovieapp/ui/InformationActivity.kt
+++ app/src/main/java/uz/com/simplemovieapp/ui/InformationActivity.kt
@@ -2,11 +2,19 @@ package uz.com.simplemovieapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_information.*
 import uz.com.simplemovieapp.R
+import uz.com.simplemovieapp.models.MovieModel
 
 class InformationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_information)
+        val information = intent.getSerializableExtra("key") as MovieModel
+        name_use.text = information.name
+        name_1.text = "Movie name: ${information.name}"
+        authors1.text ="Movie authors: ${information.authors}"
+        about1.text = "About movie: ${information.about}"
+        date1.text = "Date: ${information.date}"
     }
 }
